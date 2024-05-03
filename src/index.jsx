e0891f31a06460b3e1b04c5b944cda3289e22b28
samakunchan <badgehades@hotmail.com>
2024-05-04 01:19:39 +0200

    feat: router
    - Installation du package react router dom pour avoir le <Link />
    - Configuration du router dans l'index principale

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,17 @@
 import './index.scss';
+import { BrowserRouter as Router, Routes } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+  <>
+    <Router>
+      <Routes></Routes>
+    </Router>
+  </>,
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
