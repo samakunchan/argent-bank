fc12c4e20d5a0d436c3f7268197fa6b3eb62d281
samakunchan <badgehades@hotmail.com>
2024-05-16 21:01:36 +0200

    feat: loading
    - Création d'un composant loader

---
+++ src/components/bloc/LoadingComponent/index.jsx
@@ -0,0 +1,13 @@
+import './index.scss';
+
+const LoadingComponent = () => {
+  return (
+    <main className='main bg-dark'>
+      <div className='header'>
+        <h1>Chargement...</h1>
+      </div>
+    </main>
+  );
+};
+
+export default LoadingComponent;
